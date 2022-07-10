db1007bde92a10073031481e2c7c0b309dc74bbe
Andrey91 <mistaandrey@icloud.com>
2022-07-10 19:33:49 +0500

    1. Создаем LoginViewController 2. Добавляем различные элементы интерфейса

---
+++ 1DZ/1/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1
+//
+//  Created by User on 21.05.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
